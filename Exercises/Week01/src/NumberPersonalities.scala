683e4d9e2a1ea4bc6ef21dfc1e8a2c8973435950
vdavios <vas.davios@gmail.com>
2017-03-16 12:18:23 +0000

    Added triangular and square numbers.

--- Exercises/Week01/src/NumberPersonalities.scala
+++ Exercises/Week01/src/NumberPersonalities.scala
@@ -4,17 +4,22 @@ object NumberPersonalities  {
   val limit = 100
 
   def main(args: Array[String]): Unit ={
-    for(i <- 1 to limit) {
+    /*for(i <- 1 to limit) {
      println(s"$i  ${if (isPrime(i)) "p, " else "c, "}" +
        s"${if(isHappy(i))"h, "else "u, "}" +
        s"${if(triangular(i))"t, "else "not t"}")
 
 
+    }*/
+
+    for (i <- 1 to 100){
+
+      println(s"$i ${if(isSquare(i))"s, "else "not s, "}")
     }
 
 
-  }
 
+  }
   def isPrime(n: Int): Boolean = {
     if(n == 1){
       return false
@@ -24,9 +29,7 @@ object NumberPersonalities  {
    }
     true
   }
-
   def isHappy(n: Int): Boolean = {
-    var found = false
     var k = n
     var result = 0
     while(k>0) {
@@ -37,24 +40,36 @@ object NumberPersonalities  {
         result = 0
       }
     }
-    if(result == 1) found =  true
+    if(result == 1) return true
 
-    if(result == 4) found = false
-    found
+    false
 
   }
-  def triangular(n: Int): Boolean = {
+  def isTriangular(n: Int): Boolean = {
     var k = n
     var subtract = 1
-    var found = false
     while(k>0) {
       k-=subtract
       subtract+=1
     }
-    if(k == 0) found = true
-    if(k < 0) found = false
-    found
+    if(k == 0)return true
+
+    false
   }
+
+  def isSquare(n: Int): Boolean = {
+
+    var k = n
+    var subtract = 1
+    while (k>0){
+      k-=subtract
+      subtract+=2
+    }
+    if(k == 0) return true
+
+    false
+  }
+
 
 
 

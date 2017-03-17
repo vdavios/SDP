c8580a92951bf459c2e8c0cd43b5f70ee997ab6a
vdavios <vas.davios@gmail.com>
2017-03-17 17:15:01 +0000

    Restructure of repo

---
+++ Exercises/Week01/Scala/src/Computation.scala
@@ -0,0 +1,20 @@
+/**
+  * After loading computation.scala at REPL
+  try isEven(9), isEven(8), isEven(120).
+  *
+  * or you can just copy and paste the code
+  * :paste and after you are done press ctrl D
+  *
+  * println("2+2 = " + 2+2)
+    println("13 * 13 = " +13 * 13)
+    def isEven(number : Int ) : Boolean = {
+        number % 2 ==0
+    }
+  *
+  *
+  *
+  *
+  *
+  */
+
+
